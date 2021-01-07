040ced78c11f3b49731e5519e5a9db981c4e972d
Francis Nimpagaritse <franimpa@yahoo.fr>
2021-01-07 20:06:47 +0100

    Spaces arranging

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -17,8 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Proxies\__CG__\App\Entity\User as EntityUser;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -14,7 +14,6 @@ class PictureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('name', FileType::class, array('data_class' => null))
             ->add('imageName', FileType::class, [
                 'mapped' => false, 
                 'required' => false])

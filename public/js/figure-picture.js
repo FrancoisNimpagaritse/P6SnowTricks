4d23d4f58dc3dab72e3f93067570ebd41afbb58b
Francis Nimpagaritse <franimpa@yahoo.fr>
2021-01-07 19:14:43 +0100

    String quoted in js files

--- public/js/figure-picture.js
+++ public/js/figure-picture.js
@@ -1,14 +1,14 @@
-$('#add-picture').click(function(){
+$("#add-picture").click(function(){
     //Calculer le numéro du futur champ à créer
-    const index = +$('#widgets-counter').val();
+    const index = +$("#widgets-counter").val();
     
     //Récupérer le prototype/le code html d'une entrée
-    const tmpl = $('#figure_pictures').data('prototype').replace(/__name__/g, index);
+    const tmpl = $("#figure_pictures").data('prototype').replace(/__name__/g, index);
 
     //Injecter ce doe html dans la div add_images
-    $('#figure_pictures').append(tmpl);
+    $("#figure_pictures").append(tmpl);
 
-    $('#widgets-counter').val(index + 1);
+    $("#widgets-counter").val(index + 1);
     //On gère le bouton supprimer
     handleDeleteButtons();            
 });
@@ -23,8 +23,8 @@ function handleDeleteButtons()
 
 function updateCounter()
 {
-    const count = +$('#figure_pictures div.form-group').length;
-     $('#widgets-counter').val(count);
+    const count = +$("#figure_pictures div.form-group").length;
+     $("#widgets-counter").val(count);
 }
 
 updateCounter();
